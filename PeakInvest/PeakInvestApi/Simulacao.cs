94aea9820eb6937e1276e3d59af4d601ed7fc2e3
Adiel Silva <adiel.793@gmail.com>
2021-03-16 21:37:28 -0300

    Commit Inicial

---
+++ PeakInvest/PeakInvestApi/Simulacao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeakInvestApi
+{
+    public class Simulacao
+    {
+        public int QtdParcelas { get; set; }
+        public double Valor { get; set; }
+    }
+}
